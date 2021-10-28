951add03895c19437180d22ccd8b1bbf1caef1ba
Nguyen Manh Tuan Anh <anhng.dev@gmail.com>
2021-10-28 10:37:53 +0700

    Update gorm

--- go.mod
+++ go.mod
@@ -8,7 +8,12 @@ require (
 )
 
 require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.2 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/pierrec/lz4 v2.6.1+incompatible // indirect
+	gorm.io/driver/mysql v1.1.3 // indirect
+	gorm.io/gorm v1.22.1 // indirect
 )
--- main.go
+++ main.go
@@ -8,8 +8,11 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/segmentio/kafka-go"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 
 	"github.com/xdorro/golang-kafka-example/handler"
+	"github.com/xdorro/golang-kafka-example/model"
 )
 
 const (
@@ -21,11 +24,25 @@ const (
 func main() {
 	// create a new context
 	ctx := context.Background()
+
+	db, err := gorm.Open(
+		mysql.Open("root:123456aA@@tcp(localhost:3306)/kafka?charset=utf8&parseTime=True&loc=Local"),
+		&gorm.Config{},
+	)
+	if err != nil {
+		log.Fatalf("gorm.Open(): %v", err)
+	}
+
+	// Migrate the schema
+	if err = db.AutoMigrate(&model.Sync{}); err != nil {
+		log.Fatalf("db.AutoMigrate(): %v", err)
+	}
+
 	// produce messages in a new go routine, since
 	// both the produce and consume functions are
 	// blocking
 	go produce(ctx)
-	consume(ctx)
+	consume(ctx, db)
 }
 
 func produce(ctx context.Context) {
@@ -34,7 +51,7 @@ func produce(ctx context.Context) {
 	defer func(writer *kafka.Writer) {
 		err := writer.Close()
 		if err != nil {
-			log.Fatalf("handler writer closed: %v", err)
+			log.Fatalf("writer.Close(): %v", err)
 		}
 	}(writer)
 
@@ -47,7 +64,7 @@ func produce(ctx context.Context) {
 		}
 		err := writer.WriteMessages(ctx, msg)
 		if err != nil {
-			fmt.Println(err)
+			log.Fatalf("writer.WriteMessages(): %v", err)
 		} else {
 			fmt.Printf("produced at key:%v value:%v\n", key, string(msg.Value))
 		}
@@ -55,13 +72,13 @@ func produce(ctx context.Context) {
 	}
 }
 
-func consume(ctx context.Context) {
+func consume(ctx context.Context, db *gorm.DB) {
 	reader := handler.NewConsumer(kafkaURL, topic, groupID)
 
 	defer func(reader *kafka.Reader) {
 		err := reader.Close()
 		if err != nil {
-			log.Fatalf("handler reader closed: %v", err)
+			log.Fatalf("reader.Close(): %v", err)
 		}
 	}(reader)
 
@@ -69,8 +86,12 @@ func consume(ctx context.Context) {
 	for {
 		m, err := reader.ReadMessage(ctx)
 		if err != nil {
-			log.Fatalln(err)
+			log.Fatalf("reader.ReadMessage(): %v", err)
 		}
-		fmt.Printf("message at topic:%v partition:%v offset:%v	%s = %s\n", m.Topic, m.Partition, m.Offset, string(m.Key), string(m.Value))
+
+		key := string(m.Key)
+		val := string(m.Value)
+		db.Create(&model.Sync{Key: key, Value: val})
+		fmt.Printf("message at topic:%v partition:%v offset:%v	%s = %s\n", m.Topic, m.Partition, m.Offset, key, val)
 	}
 }
