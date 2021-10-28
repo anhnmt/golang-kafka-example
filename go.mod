d223d5c0ddc273ef169f97787d660aa36405e9fb
Nguyen Manh Tuan Anh <anhng.dev@gmail.com>
2021-10-28 10:19:40 +0700

    Update kafka

--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module github.com/xdorro/golang-kafka-example
 go 1.17
 
 require (
+	github.com/google/uuid v1.3.0
+	github.com/segmentio/kafka-go v0.4.22
+)
+
+require (
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/pierrec/lz4 v2.6.1+incompatible // indirect
-	github.com/segmentio/kafka-go v0.4.19 // indirect
 )
--- main.go
+++ main.go
@@ -4,16 +4,18 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
-	"strconv"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/segmentio/kafka-go"
+
+	"github.com/xdorro/golang-kafka-example/handler"
 )
 
 const (
-	topic         = "message-log"
-	brokerAddress = "localhost:19092"
+	kafkaURL = "localhost:19092"
+	topic    = "message-log"
+	groupID  = "handler-group"
 )
 
 func main() {
@@ -27,66 +29,48 @@ func main() {
 }
 
 func produce(ctx context.Context) {
-	// to create topics when auto.create.topics.enable='true'
-	conn, err := kafka.DialLeader(context.Background(), "tcp", brokerAddress, topic, 0)
-	if err != nil {
-		panic(err.Error())
-	}
-	defer conn.Close()
-	// initialize a counter
-	i := 0
+	writer := handler.NewProducer(kafkaURL, topic)
 
-	l := log.New(os.Stdout, "kafka writer: ", 0)
-	// intialize the writer with the broker addresses, and the topic
-	w := kafka.NewWriter(kafka.WriterConfig{
-		Brokers: []string{brokerAddress},
-		Topic:   topic,
-		// assign the logger to the writer
-		Logger: l,
-	})
-
-	for {
-		// each kafka message has a key and value. The key is used
-		// to decide which partition (and consequently, which broker)
-		// the message gets published on
-		err := w.WriteMessages(ctx, kafka.Message{
-			Key: []byte(strconv.Itoa(i)),
-			// create an arbitrary message payload for the value
-			Value: []byte("this is message" + strconv.Itoa(i)),
-		})
+	defer func(writer *kafka.Writer) {
+		err := writer.Close()
 		if err != nil {
-			panic("could not write message " + err.Error())
+			log.Fatalf("handler writer closed: %v", err)
 		}
+	}(writer)
 
-		// log a confirmation once the message is written
-		fmt.Println("writes:", i)
-		i++
-		// sleep for a second
-		time.Sleep(time.Second)
+	fmt.Println("start producing ... !!")
+	for i := 0; ; i++ {
+		key := fmt.Sprintf("Key-%d", i)
+		msg := kafka.Message{
+			Key:   []byte(key),
+			Value: []byte(fmt.Sprint(uuid.New())),
+		}
+		err := writer.WriteMessages(ctx, msg)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Printf("produced at key:%v value:%v\n", key, string(msg.Value))
+		}
+		time.Sleep(5 * time.Second)
 	}
 }
 
 func consume(ctx context.Context) {
-	// create a new logger that outputs to stdout
-	// and has the `kafka reader` prefix
-	l := log.New(os.Stdout, "kafka reader: ", 0)
-	// initialize a new reader with the brokers and topic
-	// the groupID identifies the consumer and prevents
-	// it from receiving duplicate messages
-	r := kafka.NewReader(kafka.ReaderConfig{
-		Brokers: []string{brokerAddress},
-		Topic:   topic,
-		GroupID: "my-group",
-		// assign the logger to the reader
-		Logger: l,
-	})
-	for {
-		// the `ReadMessage` method blocks until we receive the next event
-		msg, err := r.ReadMessage(ctx)
+	reader := handler.NewConsumer(kafkaURL, topic, groupID)
+
+	defer func(reader *kafka.Reader) {
+		err := reader.Close()
 		if err != nil {
-			panic("could not read message " + err.Error())
+			log.Fatalf("handler reader closed: %v", err)
 		}
-		// after receiving the message, log its value
-		fmt.Println("received: ", string(msg.Value))
+	}(reader)
+
+	fmt.Println("start consuming ... !!")
+	for {
+		m, err := reader.ReadMessage(ctx)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Printf("message at topic:%v partition:%v offset:%v	%s = %s\n", m.Topic, m.Partition, m.Offset, string(m.Key), string(m.Value))
 	}
 }
